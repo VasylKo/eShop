c5d661192b466335a5f23d513e1db2af9f6b356c
Vasyl Kotsiuba <clic@ukr.net>
2015-11-06 08:56:27 +0200

    Initial Commit

---
+++ eShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eShop
+//
+//  Created by Vasyl Kotsiuba on 11/6/15.
+//  Copyright © 2015 Vasyl Koysiuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ eShop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  eShop
+//
+//  Created by Vasyl Kotsiuba on 11/6/15.
+//  Copyright © 2015 Vasyl Koysiuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
