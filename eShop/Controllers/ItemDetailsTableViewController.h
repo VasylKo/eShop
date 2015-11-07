a7bfdcb26a59c894aec4e8539460add322697435
Vasyl Kotsiuba <clic@ukr.net>
2015-11-07 10:49:38 +0200

    added Item details VC

---
+++ eShop/Controllers/ItemDetailsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemDetailsTableViewController.h
+//  eShop
+//
+//  Created by Vasyl Kotsiuba on 11/7/15.
+//  Copyright © 2015 Vasyl Koysiuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemDetailsTableViewController : UITableViewController
+
+@end
